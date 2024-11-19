c2716da34c0655987fbd36b7d488ef14d14735c9
Jack Raymond <10591246+jackraymond@users.noreply.github.com>
2024-11-19 10:11:57 -0800

    Add option to disable caching

--- tutorial_code/example1_1_fm_loop_balancing.py
+++ tutorial_code/example1_1_fm_loop_balancing.py
@@ -169,7 +169,8 @@ def run_iteration(param, shim, stats, embeddings):
     stats['all_alpha_J'].append(shim['alpha_J'])
 
 
-def run_experiment(param, shim, stats, embeddings, alpha_Phi=0., alpha_J=0.):
+def run_experiment(param, shim, stats, embeddings, alpha_Phi=0., alpha_J=0.,
+                   use_cache=True):
     """Run the full experiment
 
     Args:
@@ -179,14 +180,21 @@ def run_experiment(param, shim, stats, embeddings, alpha_Phi=0., alpha_J=0.):
         shim (dict): shimming data
         stats (dict): dict of sampled statistics
         embeddings (List[dict]): list of embeddings
-        alpha_Phi (float, optional): learning rate for linear shims. Defaults to 0.
-        alpha_J (float, optional): learning rate for coupling shims. Defaults to 0.
+        alpha_Phi (float): learning rate for linear shims. Defaults to 0.
+        alpha_J (float): learning rate for coupling shims. Defaults to 0.
+        use_cache (bool): When True an attempt is made to load (save) data from
+            (to) the directory cached_experimental_data.
+    Returns:
+       dict: experiment statistics
     """
-    solver_name = param['sampler'].properties['chip_id']
-    prefix = f'{solver_name}_example1_1_aPhi{alpha_Phi}_aJ{alpha_J}'
+    if use_cache:
+        solver_name = param['sampler'].properties['chip_id']
+        prefix = f'{solver_name}_example1_1_aPhi{alpha_Phi}_aJ{alpha_J}'
+        data_dict = {'param': param, 'shim': shim, 'stats': stats}
+        data_dict = load_experiment_data(prefix, data_dict)
+    else:
+        data_dict = None
 
-    data_dict = {'param': param, 'shim': shim, 'stats': stats}
-    data_dict = load_experiment_data(prefix, data_dict)
     if data_dict is not None:
         param = data_dict['param']
         shim = data_dict['shim']
@@ -202,11 +210,11 @@ def run_experiment(param, shim, stats, embeddings, alpha_Phi=0., alpha_J=0.):
 
             shim['alpha_J'] = alpha_J
             run_iteration(param, shim, stats, embeddings)
-
-        save_experiment_data(
-            prefix,
-            {'param': param, 'shim': shim, 'stats': stats}
-        )
+        if use_cache:
+            save_experiment_data(
+                prefix,
+                {'param': param, 'shim': shim, 'stats': stats}
+            )
 
     return {
         'alpha_Phi': alpha_Phi,
@@ -215,7 +223,7 @@ def run_experiment(param, shim, stats, embeddings, alpha_Phi=0., alpha_J=0.):
     }
 
 
-def main(solver_name=None, coupling=-0.2, num_iters=100, num_iters_unshimmed_flux=10):
+def main(solver_name=None, coupling=-0.2, num_iters=100, num_iters_unshimmed_flux=10, use_cache=True):
     """Main function to run example.
 
     Completes an experiment matched to Figure 6 of DOI10.3389/fcomp.2023.1238988,
@@ -229,6 +237,10 @@ def main(solver_name=None, coupling=-0.2, num_iters=100, num_iters_unshimmed_flu
         num_iters (int): Total number of programmings (iterations). Defaults to 300.
         num_iters_unshimmed_flux (int): Number of iterations without shimming of flux_biases. Defaults to 100.
         num_iters_unshimmed_J (int): Number of iterations without shimming of couplings. Defaults to 200.
+        use_cache (bool): When True embeddings and data are read from
+            (and saved to) local directories, repeated executions can reuse 
+            collected data. When False embeddings and data are recalculated on
+            each call. Defaults to True
     """
     if solver_name == 'MockDWaveSampler':
         sampler_instance = ShimmingMockSampler()
@@ -246,7 +258,7 @@ def main(solver_name=None, coupling=-0.2, num_iters=100, num_iters_unshimmed_flu
             'num_iters_unshimmed_flux': num_iters_unshimmed_flux
         }
 
-        embeddings = embed_loops(sampler=sampler, L=param['L']) 
+        embeddings = embed_loops(sampler=sampler, L=param['L'], use_cache=use_cache) 
 
         # Where the shim data (parameters and Hamiltonian terms) are stored
         shim = {
@@ -266,7 +278,8 @@ def main(solver_name=None, coupling=-0.2, num_iters=100, num_iters_unshimmed_flu
             'all_alpha_J': []
         }
 
-        experiment_data = run_experiment(param, shim, stats, embeddings, alpha_Phi, .0)
+        experiment_data = run_experiment(param, shim, stats, embeddings,
+                                         alpha_Phi, .0, use_cache)
         results.append(experiment_data)
 
     paper_plots_example1_1(
